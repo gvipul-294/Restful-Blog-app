7b84eb5b9c5c5c31bd3447e1238af164e066529f
gvipul-294 <gvipul294@gmail.com>
2018-01-31 21:57:32 +0530

    simple restful blog app

--- README.md
+++
@@ -1,2 +0,0 @@
-# repBack
-My first Express App.
--- Restful BlogApp/app.js
+++ Restful BlogApp/app.js
@@ -1,22 +1,21 @@
-var bodyParser    =require("body-parser");
- var mongoose	  =require("mongoose");
- var express		  =require("express");
- var app			  =express();
+var bodyParser    =require("body-parser"),
+  mongoose	  =require("mongoose"),
+  express		  =require("express"),
+	 app			  =express();
 
-mongoose.createConnection('mongodb://localhost/restfulblog_app', {
+/*mongoose.createConnection('mongodb://localhost/restfulblog_app', {
   useMongoClient: true
-  /* other options */
-});
+  // other options 
+}); */
+mongoose.connect("mongodb://localhost:27017/restfulblog_app");
+mongoose.connection.on('open', () => {
+  console.log('Connected to mongodb server.');
+  
+})
 //APP CONFIG
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
-app.get('/',function(req,res){
-	res.redirect("/blogs");
-})
-app.get('/blogs',function(req,res){
-	res.render("index");
-})
 //MONGOOSE MODEL CONFIG
 var blogSchema=new mongoose.Schema({
 	title:String,
@@ -29,10 +28,52 @@ var blogSchema=new mongoose.Schema({
 
 });
 var Blog=mongoose.model("Blog", blogSchema);
-Blog.create({
+/*Blog.create({
 	title:"Test Blog",
 	image:"https://upload.wikimedia.org/wikipedia/en/1/17/Batman-BenAffleck.jpg",
 	body:"HELLO, welcome to the blog post"
+});*/
+app.get('/',function(req,res){
+	res.redirect("/blogs");
+})
+app.get('/blogs',function(req,res){
+	Blog.find({}, function(err,blogs){
+		if(err){
+			console.log("ERROR!");
+		}
+		else{
+			res.render("index",{blogs: blogs});
+		}
+	});
+	
+});
+app.get("/blogs/new", function(req, res){
+	res.render("new");
 });
 
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+//create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		}
+		else{
+			//then redirect to the index
+			res.redirect("/blogs");
+		}
+	});
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req,res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else{
+			res.render("show", {blog: foundBlog});
+		}
+	})
+});
 app.listen(3000);
