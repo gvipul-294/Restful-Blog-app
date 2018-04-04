9434cf33081c2c3fa38cf5c86bae2bbbcd376d29
gvipul-294 <gvipul294@gmail.com>
2018-04-04 23:32:03 +0530

    Edit and Delete operations completed

--- Restful BlogApp/app.js
+++ Restful BlogApp/app.js
@@ -1,5 +1,6 @@
 var bodyParser    =require("body-parser"),
   mongoose	  =require("mongoose"),
+  methodOverride= require("method-override"),
   express		  =require("express"),
 	 app			  =express();
 
@@ -16,6 +17,7 @@ mongoose.connection.on('open', () => {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 //MONGOOSE MODEL CONFIG
 var blogSchema=new mongoose.Schema({
 	title:String,
@@ -77,7 +79,40 @@ app.get("/blogs/:id", function(req,res){
 	})
 });
 
+//EDIT ROUTE
 app.get("/blogs/:id/edit", function(req, res){
-			res.render("edit");
+			Blog.findById(req.params.id, function(err, foundBlog){
+				if(err){
+					res.redirect("/blogs");
+				}
+				else{
+					res.render("edit", {blog: foundBlog});
+				}
+			});
+			
+})
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req,res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, UpdatedBlog){
+			if(err){
+				res.redirect("/blogs");
+			}else{
+				res.redirect("/blogs/"+req.params.id);
+			}
+	});
 });
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req,res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else{
+			res.redirect("/blogs");	
+		}
+	})
+});
+
 app.listen(3000);
